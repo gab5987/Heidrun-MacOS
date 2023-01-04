9b936c596d20d43fcdb6abf3f4ccf6a8f0269877
gab5987 <biell.gb4@gmail.com>
2023-01-04 09:47:07 -0300

    Initial Commit

---
+++ Heidrun MacOS Client/Heidrun_MacOS_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heidrun_MacOS_ClientApp.swift
+//  Heidrun MacOS Client
+//
+//  Created by Gabriel on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Heidrun_MacOS_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
