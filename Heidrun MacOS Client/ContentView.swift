d7913844f787aa763cf1c9bcb2ddab51835b2dea
gab5987 <biell.gb4@gmail.com>
2023-01-04 10:40:31 -0300

    Feat: Added sidebar

--- Heidrun MacOS Client/ContentView.swift
+++ Heidrun MacOS Client/ContentView.swift
@@ -9,15 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+        NavigationView {
+            SideBar();
+            
+            MainContent();
+        }
+        .frame(minWidth: 600, minHeight: 400);
+        //.padding()
+    }
+}
+
+struct MainContent: View {
+    var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Trying some stuff yk")
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
