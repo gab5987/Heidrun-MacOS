acb66be571c65dc3d1382ee5e71bf25d114c817c
gab5987 <biell.gb4@gmail.com>
2023-01-06 20:03:51 -0300

    Feat: Bridging for objective C++

---
+++ Heidrun MacOS Client/ServerGate.h
@@ -0,0 +1,21 @@
+//
+//  ServerGate.h
+//  Heidrun MacOS Client
+//
+//  Created by Gabriel on 06/01/23.
+//
+
+#ifndef ServerGate_h
+#define ServerGate_h
+
+#import <Foundation/Foundation.h>
+
+@interface ServerGate : NSObject
+
+// @property (strong, nonatomic) id someProperty;
+
+- (int) someMethod;
+
+@end
+
+#endif /* ServerGate_h */
---
+++ Heidrun MacOS Client/ServerGate.mm
@@ -0,0 +1,18 @@
+//
+//  ServerGate.m
+//  Heidrun MacOS Client
+//
+//  Created by Gabriel on 06/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ServerGate.h"
+
+@implementation ServerGate
+
+- (int) someMethod {
+    return 12;
+}
+
+@end
